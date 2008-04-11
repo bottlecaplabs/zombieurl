20b8a06fc99dc10c47930eff2e143eb94194b2c6
Greg Borenstein <greg@grabb.it>
2008-04-11 12:31:22 -0700

    working on uglying up the image loading

--- public/javascripts/jquery.lightbox-0.4.js
+++ public/javascripts/jquery.lightbox-0.4.js
@@ -22,17 +22,17 @@
 		// Settings to configure the jQuery lightBox plugin how you like
 		settings = jQuery.extend({
 			// Configuration related to overlay
-			overlayBgColor: 		'#000',		// (string) Background color to overlay; inform a hexadecimal value like: #RRGGBB. Where RR, GG, and BB are the hexadecimal values for the red, green, and blue values of the color.
-			overlayOpacity:			0.8,		// (integer) Opacity value to overlay; inform: 0.X. Where X are number from 0 to 9
+			// overlayBgColor: 		'#000',		// (string) Background color to overlay; inform a hexadecimal value like: #RRGGBB. Where RR, GG, and BB are the hexadecimal values for the red, green, and blue values of the color.
+			// overlayOpacity:			0.8,		// (integer) Opacity value to overlay; inform: 0.X. Where X are number from 0 to 9
 			// Configuration related to images
-			imageLoading:			'images/lightbox-ico-loading.gif',		// (string) Path and the name of the loading icon
-			imageBtnPrev:			'images/lightbox-btn-prev.gif',			// (string) Path and the name of the prev button image
-			imageBtnNext:			'images/lightbox-btn-next.gif',			// (string) Path and the name of the next button image
-			imageBtnClose:			'images/lightbox-btn-close.gif',		// (string) Path and the name of the close btn
-			imageBlank:				'images/lightbox-blank.gif',			// (string) Path and the name of a blank image (one pixel)
+			//imageLoading:			'images/lightbox-ico-loading.gif',		// (string) Path and the name of the loading icon
+			//imageBtnPrev:			'images/lightbox-btn-prev.gif',			// (string) Path and the name of the prev button image
+			//imageBtnNext:			'images/lightbox-btn-next.gif',			// (string) Path and the name of the next button image
+			//imageBtnClose:			'images/lightbox-btn-close.gif',		// (string) Path and the name of the close btn
+			//imageBlank:				'images/lightbox-blank.gif',			// (string) Path and the name of a blank image (one pixel)
 			// Configuration related to container image box
 			containerBorderSize:	10,			// (integer) If you adjust the padding in the CSS for the container, #lightbox-container-image-box, you will need to update this value
-			containerResizeSpeed:	400,		// (integer) Specify the resize duration of container image. These number are miliseconds. 400 is default.
+			containerResizeSpeed:	0,		// (integer) Specify the resize duration of container image. These number are miliseconds. 400 is default.
 			// Configuration related to texts in caption. For example: Image 2 of 8. You can alter either "Image" and "of" texts.
 			txtImage:				'Image',	// (string) Specify text "Image"
 			txtOf:					'of',		// (string) Specify text "of"
@@ -128,11 +128,9 @@
 			var arrPageSizes = ___getPageSize();
 			// Style overlay and show it
 			$('#jquery-overlay').css({
-				backgroundColor:	settings.overlayBgColor,
-				opacity:			settings.overlayOpacity,
 				width:				arrPageSizes[0],
 				height:				arrPageSizes[1]
-			}).fadeIn();
+			})
 			// Get page scroll
 			var arrPageScroll = ___getPageScroll();
 			// Calculate top and left offset for the jquery-lightbox div object and show it
@@ -221,10 +219,7 @@
 		 */
 		function _show_image() {
 			$('#lightbox-loading').hide();
-			$('#lightbox-image').fadeIn(function() {
-				_show_image_data();
-				_set_navigation();
-			});
+			$('#lightbox-image').show();
 			_preload_neighbor_images();
 		};
 		/**
