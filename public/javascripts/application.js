13a92b88f93530ec8d835d6f3db0aa3dff1307e1
Adam Greene <adam@sweetspot.dm>
2008-04-11 12:27:43 -0700

    adding javascript to delay loading of image

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,9 @@
 var Zombie = {};
-// Zombie.load_delayed_images = function(){
-//    $('a#zombie').each(function(i, a){
-//       img = $("<img src='" + $(a).attr('href') + "'></img>");
-//       //NOTE: usiing img.load(); doesn't work..... it throws an erro in jquery, though this does.  odd
-//       img.trigger('load');
-//    });
-// }
-
+Zombie.delay = 1500;
 $(function() {
-   Zombie.load_delayed_images();
-   $('a#zombie').lightBox().click();
+   $('iframe.viewing').load(function(){
+      setTimeout(function(){
+         $('a#zombie').lightBox().click();         
+      }, Zombie.delay)
+   });
 });
