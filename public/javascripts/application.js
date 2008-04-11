c79bf00f9a00cab79fd9372200ed4e081b424129
Chris Anderson <jchris@grabb.it>
2008-04-11 12:56:29 -0700

    dropped local variable conceit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,17 +10,15 @@ $(function() {
 
 // for audio starting
 $(function() {
-  var sm2 = new SoundManager();
-  window.soundManager = sm2;
-  sm2.url = '/images/sm2.swf';
-  sm2.nullURL = '/images/null.mp3';
-  sm2.debugMode = false;
-  sm2.consoleOnly = true;
-  $(window).unload(sm2.destruct)
+  window.soundManager = new SoundManager();
+  soundManager.url = '/images/sm2.swf';
+  soundManager.nullURL = '/images/null.mp3';
+  soundManager.debugMode = false;
+  soundManager.consoleOnly = true;
+  $(window).unload(soundManager.destruct)
   sm2.onload=function() {
-    console.log('tog')
-    sm2.createSound('grr','/sounds/grar.mp3');
-    sm2.play('grr');
+    soundManager.createSound('grr','/sounds/grar.mp3');
+    soundManager.play('grr'); // call this when you want it to play
   };
-  sm2.beginInit();
+  soundManager.beginInit();
 });
