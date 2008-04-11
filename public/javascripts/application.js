80dedc0bf7ba8b644dc87559060b40e66b406372
Adam Greene <adam@sweetspot.dm>
2008-04-11 12:45:49 -0700

    making sure that if the loading of the iframe fails or takes forever, that we still load the image.  Also leave hooks in for caching.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,29 @@
-var Zombie = {};
-Zombie.delay = 1500;
+var Zombie = {
+   viewing_delay: 800,  // the delay AFTER the iframe is loaded
+   final_delay:   2500, // the finaly delay; we will always scare the shit out of someone after this time delay ;)
+   infected:      false // only need to infect them once.
+};
+
+Zombie.prep = function(){
+   // pre-cache lightbox and all the other stuff we need to do
+};
+
+Zombie.blight = function(){
+   if (Zombie.infected) return;
+   $('a#zombie').lightBox().click();        
+   Zombie.infected = true;
+};
+
 $(function() {
+   Zombie.prep();
+   
+   setTimeout(function(){
+      Zombie.blight();
+   }, Zombie.final_delay );
+
    $('iframe.viewing').load(function(){
       setTimeout(function(){
-         $('a#zombie').lightBox().click();         
-      }, Zombie.delay)
+         Zombie.blight();
+      }, Zombie.viewing_delay);
    });
 });
