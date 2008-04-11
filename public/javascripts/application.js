ca260c48ffdbed5cffd68374603e1e2ebd42989d
Chris Anderson <jchris@grabb.it>
2008-04-11 12:54:14 -0700

    audio grar

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,3 +7,20 @@ $(function() {
       }, Zombie.delay)
    });
 });
+
+// for audio starting
+$(function() {
+  var sm2 = new SoundManager();
+  window.soundManager = sm2;
+  sm2.url = '/images/sm2.swf';
+  sm2.nullURL = '/images/null.mp3';
+  sm2.debugMode = false;
+  sm2.consoleOnly = true;
+  $(window).unload(sm2.destruct)
+  sm2.onload=function() {
+    console.log('tog')
+    sm2.createSound('grr','/sounds/grar.mp3');
+    sm2.play('grr');
+  };
+  sm2.beginInit();
+});
